aeaca9bfb65287784443b5c5ba6c1c59822f62f4
Bill Butts <rwbutts@gmail.com>
2025-06-25 20:22:12 -0400

    minor tweaks

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ new Vue({
   el: '#root'
 });
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
+
+
 if( isPWA() ) {
     console.log("Running as PWA");
 } else {
